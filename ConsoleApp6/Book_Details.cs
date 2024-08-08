078774eb38bbde1a03a347f6dd338d7ed5f1c03e
108815 <108815@mgt.sjp.ac.lk>
2024-08-08 20:22:33 +0530

    Add project files.

---
+++ ConsoleApp6/Book_Details.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Book_Details
+    {
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+        public string BookAuthor { get; set; }
+        public int NoOfCopies { get; set; }
+
+        public Book_Details(int bookId,string bookName, string bookAuthor, int noOfCopies) 
+        {
+            BookId = bookId;
+            BookName = bookName;
+            BookAuthor = bookAuthor;
+            NoOfCopies = noOfCopies;
+        }
+
+        public Book_Details() { }
+
+
+        //Book display
+        public override string ToString()
+        {
+            string display = $"Book id: {BookId} Book name: {BookName} Book author: {BookAuthor} No of copies: {NoOfCopies}";
+            return display;
+        }
+
+        
+        //Book search method
+        public static void BookSearch(List<Book_Details> BookStore)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter Book search type : \n1.Id \n2.book name \n3.Author\n4.Exit search \n ");
+                int SearchBook = Program.readInt("Enter search type number: ");
+                if (SearchBook == 1)
+                {
+                    int searchId = Program.readInt("Enter book id ");
+                    foreach (Book_Details book in BookStore)
+                    {
+                        if (book.BookId == searchId)
+                        {
+                            Console.WriteLine(book);
+                        }
+                        
+                    }
+                }
+                else if (SearchBook == 2)
+                {
+                    string searchBookName = Program.read("Enter book name ");
+                    foreach (Book_Details book in BookStore)
+                    {
+                        if (book.BookName.Contains(searchBookName))
+                        {
+                            Console.WriteLine(book);
+                        }
+                    }
+                }
+                else if (SearchBook == 3)
+                {
+                    string searchBookAuthor = Program.read("Enter book author ");
+                    foreach (Book_Details book in BookStore)
+                    {
+                        if (book.BookAuthor.Contains(searchBookAuthor))
+                        {
+                            Console.WriteLine(book);
+                        }
+                    }
+                }
+                else if (SearchBook==4)
+                {
+                    break;
+                }
+                else { break; }
+            }
+
+
+
+        }
+
+        //Book delete method
+        public static void BookDelete(List<Book_Details> BookStore)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter Book delete type : \n1.Id \n2.book name \n3.Exit delete \n ");
+                int DeleteBook = Program.readInt("Enter delete type number: ");
+
+                if (DeleteBook == 1)
+                {
+                    int BookDeleteId = Program.readInt("Book delete \nEnter book id: ");
+                    for (int i = 0; i < BookStore.Count; i++)
+                    {
+                        if (BookStore[i].BookId == BookDeleteId)
+                        {
+                            BookStore.RemoveAt(i);
+                            Console.WriteLine("Book deleted");
+                        }
+                    }
+                }
+                else if (DeleteBook == 2)
+                {
+                    string BookDeleteName = Program.read("Book delete \nEnter book name: ");
+                    for (int i = 0; i < BookStore.Count; i++)
+                    {
+                        if (BookStore[i].BookName == BookDeleteName)
+                        {
+                            BookStore.RemoveAt(i);
+                            Console.WriteLine("Book deleted");
+                        }
+                    }
+                }
+                else if(DeleteBook == 3)
+                {
+                    break ;
+                }
+
+
+            }
+
+
+        }
+
+        //Book update method
+        public static void UpdateBookDetails(List<Book_Details> BookStore)
+        {
+            int oldBookId = Program.readInt("Update book details\nEnter current book id: ");
+
+            foreach (Book_Details book in BookStore)
+            {
+                if (book.BookId == oldBookId)
+                {
+                    string newBookId = Program.read("Enter new book id: ");
+                    string newBookName = Program.read("Enter new book name: ");
+                    string newBookAuthor = Program.read("Enter new book author: ");
+                    string newNoOfCopies = Program.read("Enter new number of copies: ");
+ 
+                    
+                    book.BookId = int.Parse(newBookId);
+                    book.BookName = newBookName;
+                    book.BookAuthor = newBookAuthor;
+                    book.NoOfCopies = int.Parse(newNoOfCopies);
+
+                    Console.WriteLine("Book details updated");
+                    break;
+                }
+            }
+        }
+
+    }
+}
+
+
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book_Details> BookStore = new List<Book_Details>();
+
+            string jsonReader = File.ReadAllText("bookStore.json");
+            BookStore = JsonSerializer.Deserialize<List<Book_Details>>(jsonReader);
+
+            while (true)
+            {
+                Console.Write("Enter operation \n1.Add book: \n2.View book: \n3.Search book: \n4.Delete book: \n5.Update book: \n6.Exit system\n");
+                int operation = readInt("Operation number : ");
+
+                //add book
+                if (operation == 1)
+                {
+                    int bookId = readInt("Enter book Id : ");
+                    string bookName = read("Enter book name : ");
+                    string bookAuthor = read("Enter book author : ");
+                    int noOfCopies = readInt("Enter no of copies : ");
+
+                    Book_Details book = new Book_Details(bookId,bookName,bookAuthor,noOfCopies);
+
+                    BookStore.Add(book);
+
+                }
+
+                //view book
+                else if(operation== 2)
+                {
+                    foreach (Book_Details book in BookStore)
+                    {
+                        Console.WriteLine(book);
+                    }
+                }
+
+                //search book
+                else if(operation == 3)
+                {
+                    Book_Details.BookSearch(BookStore);
+                }
+
+                //Delete book
+                else if(operation == 4)
+                {
+                    Book_Details.BookDelete(BookStore);  
+                }
+
+                //Update book
+                else if (operation == 5)
+                {
+                    Book_Details.UpdateBookDetails(BookStore);
+
+                }
+
+                //exit
+                else if (operation== 6)
+                {
+                    string jsonWrite = JsonSerializer.Serialize(BookStore);
+                    File.WriteAllText("bookStore.json", jsonWrite);
+                    break;
+                }
+            }
+        }
+
+        public static string read(string message)
+        {
+            Console.Write(message);
+            string read = Console.ReadLine();
+            return read;
+        }
+
+        public static int readInt(string message)
+        {
+            Console.Write(message);
+            string readInt = Console.ReadLine();
+            return int.Parse(readInt);
+        }
+    }
+}
